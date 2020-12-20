7aee1c7dd083724041c720550e48848563956da3
Francisco Rosado <Franciscorosado09@gmail.com>
2020-12-20 10:25:20 -0500

    added base html:

---
+++ style.js
@@ -0,0 +1,12 @@
+// var queryURL = api.openweathermap.org/data/2.5/weather?
+
+// id={city id}&appid={API key}
+
+
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+        
+
+
